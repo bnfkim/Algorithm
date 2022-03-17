d9e2e4d42ab02d52a63dedc2fff899224cf163a5
Gyuri Kim <kgr2626@naver.com>
2022-03-18 01:34:18 +0900

    [Bronze III] Title: N 찍기, Time: 288 ms, Memory: 20860 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -1,11 +1,22 @@
 import java.util.*;
+import java.io.*;
 public class Main {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        
-        for(int i = 1; i < N+1; i++){
-            System.out.println(i);
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+        for(int i=1; i<N+1; i++){
+            bw.write(i + "\n");
         }
+        bw.flush();
+        bw.close();
+
+        //Scanner sc = new Scanner(System.in);
+        //int N = sc.nextInt();
+
+        //for(int i = 1; i < N+1; i++){
+        //    System.out.println(i);
+        //}
     }
 }
