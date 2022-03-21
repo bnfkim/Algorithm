de5143c30c1c8ea323e7fdd79a4c5d2fb445b70d
Gyuri Kim <kgr2626@naver.com>
2022-03-22 01:53:59 +0900

    [Bronze II] Title: 최댓값, Time: 212 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[9];
+
+        for(int i =0; i<9; i++) {
+            int num = sc.nextInt();
+            arr[i] = num;
+        }
+
+        int max = 0;
+        int count = 0;
+
+        for(int i = 0; i<arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+                count = i+1;
+            }
+        }
+        System.out.println(max);
+        System.out.println(count);
+    }
+}
