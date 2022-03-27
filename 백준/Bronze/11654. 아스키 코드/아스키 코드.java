debd5f699ab099a5edcb9d9fdee8323b6cbae528
Gyuri Kim <kgr2626@naver.com>
2022-03-27 19:43:14 +0900

    [Bronze V] Title: 아스키 코드, Time: 120 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,15 +1,6 @@
-import java.io.*;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        // string 형식으로 받기 -> charAt을 통해 char로 변경 -> 변수 word 정의를 통해 int type으로 변경
-        int word = br.readLine().charAt(0);
-
-        bw.write(word + "\n");
-        br.close();
-        bw.flush();
-        bw.close();
+    public static void main(String[] args) throws Exception {
+        int a = System.in.read();
+        System.out.print(a);
     }
 }
