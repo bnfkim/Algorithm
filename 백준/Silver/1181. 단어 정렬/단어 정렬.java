2233e3359df36f30bf8ff2fff2bfb420a7f3c7ce
Gyuri Kim <kgr2626@naver.com>
2022-07-15 19:07:48 +0900

    [Silver V] Title: 단어 정렬, Time: 356 ms, Memory: 26684 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -3,8 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Objects;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -19,24 +17,24 @@ public class Main {
 
         //compare 메소드는 3가지 리턴 값에 의해 위치를 바꿀지 결정
         //3가지 리턴 값 -> 양의 정수(위치바꿈), 0(그대로), 음의 정수(그대로)
-//        Arrays.sort(arr, new Comparator<String>() {
-//            @Override
-//            public int compare(String o1, String o2) {
-//                if(o1.length() == o2.length()) { //두 번째 원소가 같다면 첫 번째 원소끼리 비교
-//                    return o1.compareTo(o2);
-//                } else {
-//                    return o1.length() - o2.length();
-//                }
-//            }
-//        });
-
-        Arrays.sort(arr, (e1, e2) -> {
-            if(e1.length() == e2.length()) { //두 번째 원소가 같다면 첫 번째 원소끼리 비교
-                return e1.compareTo(e2);
-            } else {
-                return e1.length() - e2.length();
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() == o2.length()) { //두 번째 원소가 같다면 첫 번째 원소끼리 비교
+                    return o1.compareTo(o2);
+                } else {
+                    return o1.length() - o2.length();
+                }
             }
         });
+
+//        Arrays.sort(arr, (e1, e2) -> {
+//            if(e1.length() == e2.length()) { //두 번째 원소가 같다면 첫 번째 원소끼리 비교
+//                return e1.compareTo(e2);
+//            } else {
+//                return e1.length() - e2.length();
+//            }
+//        });
 
         StringBuilder sb = new StringBuilder();
         sb.append(arr[0]).append('\n');
