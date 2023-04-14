907e9992640c4a980543284fa329423cee8d9b36
Gyuri Kim <kgr2626@naver.com>
2023-04-14 10:14:00 +0900

    [Silver V] Title: 셀프 넘버, Time: 168 ms, Memory: 14596 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -1,32 +1,20 @@
-import java.io.*;
-import java.util.ArrayList;
-
-public class Main {
-    // self num을 구하는 함수
-    public static int d(int num){
-        int sum = num;
-        while (num>0){
-            sum += num %10;
-            num = num/10;
-        }
-        return sum;
-    }
-
+public class Main{
     public static void main(String[] args) {
         boolean[] arr = new boolean[10001];
-        for (int i=1; i<10001; i++){
-            int self_num = d(i);
-            if(self_num<10001){
-                //self num인 숫자는 true로 변경
-                arr[self_num] = true;
-            }
+        for(int i=1; i<arr.length; i++){
+            int selfNum = checkSelfNum(i);
+            if(selfNum<10001) arr[selfNum] = true;
         }
-        for(int i=1; i<10001; i++){
-            // self num이 아닌 숫자는 false로 되어있음
-            // false로 되어있는 숫자만 출력
-            if(!arr[i]) {
-                System.out.println(i);
-            }
+        for(int i=1; i<arr.length; i++){
+            if(!arr[i]) System.out.println(i);
         }
+    }
+    public static int checkSelfNum(int num) {
+        int sum = num;
+        while (num > 0) {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        return sum;
     }
 }
