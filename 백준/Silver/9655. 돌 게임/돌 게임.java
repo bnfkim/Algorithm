7886381f000b372abd88c6eff333aef7f502c34e
Gyuri Kim <kgr2626@naver.com>
2023-06-13 11:58:33 +0900

    [Silver V] Title: 돌 게임, Time: 132 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 
 class Main{
     /**
-     * 11:43 -
+     * 11:43 - 11: 53
      * [규칙]
      * 1) 돌 1개 혹은 3개
      * 2) 마지막 돌 가져가는 사람이 이김
@@ -15,27 +15,6 @@ class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine()); //돌 개수
 
-        boolean isSK = false;
-
-        /**
-         * 1개 -> 상근
-         * 2개 -> 상근 > 창영
-         * 3개 -> 상근
-         * 4개 -> 상근(3) > 창영(1)
-         * 5개 -> 상근(3) > 창영(1) > 상근(1)
-         * 6개 -> 상근(3) > 창영(3)
-         */
-
-        while(n!=0){
-            if(n>3){
-                n -= 3;
-            } else {
-                n -= 1;
-            }
-            isSK = !isSK;
-        }
-
-        if(isSK) System.out.println("SK");
-        else System.out.println("CY");
+        System.out.println((n%2 == 1) ? "SK" : "CY");
     }
 }
