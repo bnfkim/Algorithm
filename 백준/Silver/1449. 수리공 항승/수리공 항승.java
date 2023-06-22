d55ea488281e155ce575eecd8706c5a7b7f271ae
Gyuri Kim <kgr2626@naver.com>
2023-06-22 14:15:58 +0900

    [Silver III] Title: 수리공 항승, Time: 128 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.java
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.java
@@ -7,45 +7,25 @@ import java.util.StringTokenizer;
 class Main{
     public static void main(String[] args) throws IOException {
         /**
-         * 1:10 -
-         *
-         * 3 2
-         * 1 5 7
-         *
-         * 9 3
-         * 1 2 3 4 5 6 7 8 9 10 14 20
-         *
-         * 123 456 789 10 14 20
+         * 1:10 - 1:50
          */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken()); //물이 새는 곳 개수
         int L = Integer.parseInt(st.nextToken()); //테이플 길이
+
         st = new StringTokenizer(br.readLine());
         int[] arr = new int[N];
         for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
 
         Arrays.sort(arr);
         int idx = 0;
-        int cnt = 0;
-        while(idx < N) {
-            //시작 파이프에서 테이프로 수리가능한 파이프 위치
-            int len = arr[idx] + L - 1;
-            for(int i=idx; i<N; i++) {
-                if(arr[i] > len) { //10 > 12
-                    idx = i;
-                    cnt++;
-                    break;
-                }else if(arr[i] == len){
-                     cnt++;
-                     idx = i+1;
-                     break;
-                }else {
-                    idx++;
-                }
-            }
-            if(idx == N) {
-                if(arr[idx-1] != len) cnt += 1;
+        int cnt  = 1; //필요한 테이프 최소 개수
+        for(int i=idx+1; i<N; i++){
+            //한 테이프로 붙일 수 있는 범위를 넘어가면 업데이트
+            if(arr[idx] + L - 1 < arr[i]) {
+                cnt++;
+                idx = i;
             }
         }
         System.out.println(cnt);
