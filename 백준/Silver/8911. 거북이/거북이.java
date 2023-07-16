ad7f1fc59e7c00e7197f5ec2bf53dec426e632ed
Gyuri Kim <kgr2626@naver.com>
2023-07-16 20:58:28 +0900

    [Silver III] Title: 거북이, Time: 404 ms, Memory: 23384 KB -BaekjoonHub

--- 백준/Silver/8911. 거북이/거북이.java
+++ 백준/Silver/8911. 거북이/거북이.java
@@ -1,52 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 class Main{
+    static int dx[] = {0, 1, 0, -1};
+    static int dy[] = {1, 0, -1, 0};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
         int T = Integer.parseInt(br.readLine());
         while (T-->0){
-            String[] move = br.readLine().split("");
+            String cmd = br.readLine();
             int d = 0; //0(북) 1(동) 2(남) 3(서)
-            int maxX=0, maxY=0, minX=0, minY=0;
-            int dx[] = {0, 1, 0, -1};
-            int dy[] = {1, 0, -1, 0};
-            int nowX = 0;
-            int nowY = 0;
+            int maxX=0, maxY=0, minX=0, minY=0, nowX = 0, nowY=0;
 
-            for(int i=0; i< move.length; i++){
-                String m = move[i];
+            for(int i=0; i< cmd.length(); i++){
+                char m = cmd.charAt(i);
                 switch (m){
-                    case "F":
+                    case 'F':
                         nowX += dx[d];
                         nowY += dy[d];
-                        maxX = Math.max(nowX, maxX);
-                        maxY = Math.max(nowY, maxY);
-                        minX = Math.min(nowX, minX);
-                        minY = Math.min(nowY, minY);
                         break;
-                    case "B":
+                    case 'B':
                         nowX -= dx[d];
                         nowY -= dy[d];
-                        maxX = Math.max(nowX, maxX);
-                        maxY = Math.max(nowY, maxY);
-                        minX = Math.min(nowX, minX);
-                        minY = Math.min(nowY, minY);
                         break;
-                    case "L":
-                        if(d == 0) d = 4;
-                        d--;
+                    case 'L':
+                        if(d == 0) d = 3;
+                        else d--;
                         break;
-                    case "R":
-                        d++;
-                        if(d == 4) d = 0;
+                    case 'R':
+                        if(d == 3) d = 0;
+                        else d++;
                         break;
                 }
+                maxX = Math.max(nowX, maxX);
+                maxY = Math.max(nowY, maxY);
+                minX = Math.min(nowX, minX);
+                minY = Math.min(nowY, minY);
             }
-            int result = (maxX - minX) * (maxY - minY);
-            System.out.println(result);
+            sb.append((maxX - minX) * (maxY - minY)).append("\n");
         }
+        System.out.println(sb);
     }
 }
