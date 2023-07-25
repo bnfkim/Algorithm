dc5eedd75ddcf96abcf74833f1451d08b8828f68
Gyuri Kim <kgr2626@naver.com>
2023-07-25 15:59:28 +0900

    [Silver I] Title: 행렬, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/1080. 행렬/행렬.java
+++ 백준/Silver/1080. 행렬/행렬.java
@@ -2,60 +2,6 @@ import java.io.*;
 import java.util.*;
 
 class Main{
-    /**
-     * 어떤 3×3크기의 부분 행렬에 있는 모든 원소를 뒤집는 것
-     *
-     * 0000     1110    1001
-     * 0010     1100    1011
-     * 0000     1110    1001
-     *
-     * 0111     1001
-     * 0101     1011
-     * 0111     1001
-     *
-     * 1001
-     * 1011
-     * 1001
-     * => 3칸꺼를 어떤 것을 먼저 하든 결과가 나옴
-     *
-     * 001 .
-     * 100 .
-     * 100
-     * 000
-     * 011
-     * 010
-     * 100
-     * 100
-     * 010
-     * 010
-     * 010
-     * 110
-     * 101
-     * 101
-     * 000
-     * 110
-     * 000
-     *
-     * 110
-     * 001
-     * 100
-     * 011
-     * 000
-     * 100
-     * 010
-     * 011
-     * 100
-     * 101
-     * 101
-     * 010
-     * 001
-     * 010
-     * 010
-     * 111
-     * 110
-     * 111
-     * 001
-     */
     static int N, M;
     static int cnt = 0;
     static int[][] arrOri;
@@ -76,7 +22,6 @@ class Main{
             String tmp = br.readLine();
             for(int j=0; j<M; j++) arrRes[i][j] = tmp.charAt(j)-'0';
         }
-
         //다르면 뒤집음
         for(int i=0; i<=N-3; i++){
             for(int j=0; j<=M-3; j++){
@@ -86,15 +31,13 @@ class Main{
                 }
             }
         }
-
+        //마지막에 달라진 곳이 있는지 한 번 더 확인
         for(int i=0; i<N; i++){
             for(int j=0; j<M; j++){
                 if(arrOri[i][j] != arrRes[i][j]) cnt = -1;
             }
         }
-
         System.out.println(cnt);
-
     }
     //뒤집는 함수
     public static void change(int y, int x) {
