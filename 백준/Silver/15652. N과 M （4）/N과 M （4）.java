f12fa2570102ca12d439556b0c3cf354b768b8b0
Gyuri Kim <kgr2626@naver.com>
2023-12-09 22:39:01 +0900

    [Silver III] Title: N과 M (4), Time: 712 ms, Memory: 23392 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int n,m;
+    public static int[] results;
+    public static boolean[] visit;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        results = new int[m];
+        visit = new boolean[n+1];
+
+        dfs(1, 0);
+    }
+    public static void dfs(int start, int dep) {
+        //k번째 노드에 도달하면 결과에 추가
+        if(dep == m) {
+            printResult();
+            return;
+        }
+
+        //현재 엘리먼트 이후 엘리먼트 탐색
+        for(int i=start; i<=n; i++) {
+            results[dep] = i;
+            dfs(i, dep+1);
+        }
+    }
+    public static void printResult() {
+        for(int r : results) {
+            System.out.print(r + " ");
+        }
+        System.out.println();
+    }
+}
