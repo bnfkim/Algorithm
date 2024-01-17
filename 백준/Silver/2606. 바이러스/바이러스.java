fa46a07f87ea3d4041210ddd6974bf4859dbe6b9
Gyuri Kim <kgr2626@naver.com>
2024-01-17 09:27:31 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,55 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int computer[][]; //그래프 배열
-    static int check[]; //방문배열
+    public static int n,m;
+    public static ArrayList<ArrayList<Integer>> map;
+    public static boolean[] visit;
+    public static int cnt = 0;
 
-    static void bfs(int start){
-        Queue<Integer> queue = new LinkedList<>();
-
-        check[start] = 1; //시작한 컴퓨터는 감염되었다고 체크
-        queue.offer(start); //큐에 start 컴퓨터 넣음
-        int cnt = 0;
-
-        while(!queue.isEmpty()){
-            int x = queue.poll();
-
-            for(int i=1; i< computer.length; i++){
-                if (computer[x][i] == 1 && check[i] != 1){
-                    queue.offer(i);
-                    check[i] = 1;
-                    cnt++;
-                }
-            }
-        }
-        System.out.println(cnt);
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int com = Integer.parseInt(br.readLine()); //컴퓨터 수
-        int net = Integer.parseInt(br.readLine()); //컴퓨터 쌍의 수
+        n = Integer.parseInt(br.readLine()); //컴퓨터의 수
+        m = Integer.parseInt(br.readLine());  //컴퓨터의 쌍
+        visit = new boolean[n+1];
+        map = new ArrayList<>();
 
-        computer = new int[com+1][com+1];
-        check = new int[com+1];
-
-        //컴퓨터 배열에 세팅
-        for (int i=0; i<net; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
+        //(1) 그래프 입력받기
+        for(int i=0; i<n+1; i++) map.add(new ArrayList<>());
+        for(int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            //무방향 그래프 희소행렬 표현 방법법
-            computer[a][b] = 1;
-            computer[b][a] = 1;
+            map.get(a).add(b);
+            map.get(b).add(a);
         }
-        bfs(1);
+        bfs();
+        System.out.println(cnt);
+    }
+
+    public static void bfs() {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        visit[1] = true;
+
+        while(!queue.isEmpty()) {
+            int now = queue.poll();
+
+            for(int i=0; i<map.get(now).size(); i++) {
+                int next = map.get(now).get(i);
+                if(visit[next]) continue;
+
+                queue.add(next);
+                visit[next] = true;
+                cnt++;
+            }
+        }
     }
 }
