22ab2a1558bd48bb88dd55ae14e79f5052119a39
Gyuri Kim <kgr2626@naver.com>
2024-01-31 20:39:02 +0900

    [D2] Title: 달팽이 숫자, Time: 104 ms, Memory: 18,368 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -1,11 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Solution {
-    static int N;
-    static int[][] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -13,59 +11,37 @@ public class Solution {
         int T = Integer.parseInt(br.readLine());
         for(int tc=1; tc<=T; tc++) {
 
-            N = Integer.parseInt(br.readLine());
-            arr = new int[N][N];
+            int N = Integer.parseInt(br.readLine());
+            int[][] map = new int[N][N];
+            boolean[][] visit = new boolean[N][N];
 
-            int num = 1;
-            int x = 0;
-            int y = 0;
+            int[] dx = {1, 0, -1, 0};
+            int[] dy = {0, 1, 0, -1};
+
             int d = 0;
-
-            int top = 0;
-            int right = N;
-            int bottom = N;
-            int left = 0;
+            int y = 0;
+            int x = 0;
+            int num = 1;
 
             while(num <= N*N) {
-                arr[y][x] = num;
-                
-                if(d%4 == 0) x++;
-                else if (d%4 == 1) y++;
-                else if (d%4 == 2) x--;
-                else if (d%4 == 3) y--;
-
-                if(x>=right) {
-                    x--;
-                    y++;
-                    top++;
-                    d++;
+                visit[y][x] = true;
+                map[y][x] = num;
+                int ny = y + dy[d];
+                int nx = x + dx[d];
+                if(nx<0 || ny<0 || nx>=N || ny>=N || visit[ny][nx]) {
+                    d = (d + 1) % 4;
+                    ny = y + dy[d];
+                    nx = x + dx[d];
                 }
-                if(y>=bottom) {
-                    y--;
-                    x--;
-                    right--;
-                    d++;
-                }
-                if(x<left) {
-                    x++;
-                    y--;
-                    bottom--;
-                    d++;
-                }
-                if(y<top) {
-                    y++;
-                    x++;
-                    left++;
-                    d++;
-                }
-
-                if(d==4) d = 0;
+                y = ny;
+                x = nx;
                 num++;
             }
+
             sb.append("#").append(tc).append("\n");
             for(int i=0; i<N; i++) {
                 for(int j=0; j<N; j++){
-                    sb.append(arr[i][j] + " ");
+                    sb.append(map[i][j] + " ");
                 }
                 sb.append("\n");
             }
