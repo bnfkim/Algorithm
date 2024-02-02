21d3acd959649070ca924f024624408bca32d34d
Gyuri Kim <kgr2626@naver.com>
2024-02-02 21:59:03 +0900

    [Gold III] Title: 드래곤 커브, Time: 136 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.java
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.java
@@ -4,23 +4,9 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-class Dot {
-    int x;
-    int y;
-    int d;
-    int g;
-    public Dot(int x, int y, int d, int g) {
-        this.x = x;
-        this.y = y;
-        this.d = d;
-        this.g = g;
-    }
-
-}
-
 public class Main {
     static int N;
-    static ArrayList<Integer> dotList;
+    static ArrayList<Integer> directions;
     static boolean[][] checked;
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, -1, 0, 1};
@@ -30,7 +16,6 @@ public class Main {
         N = Integer.parseInt(br.readLine());
         checked = new boolean[101][101];
 
-        int sum = 0;
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
@@ -38,33 +23,26 @@ public class Main {
             int d = Integer.parseInt(st.nextToken());
             int g = Integer.parseInt(st.nextToken());
 
-            getDotList(d, g);
-            checkDot(x, y);
+            getDirections(d, g);
+            setDragonCurve(x, y);
         }
         System.out.println(getDragonCurve());
     }
-    static void getDotList(int d, int g) {
-        int startGeneration = 1;
+    static void getDirections(int d, int g) {
+        directions = new ArrayList<>();
+        directions.add(d);
 
-        dotList = new ArrayList<>(); //[1] [2]
-        dotList.add(d);
-
-        while(startGeneration <= g) {
-
-            for(int i=dotList.size()-1; i>=0; i--) {
-                int nextDirection = dotList.get(i) + 1;
-                if(nextDirection == 4) nextDirection = 0;
-
-                dotList.add(nextDirection);
+        for (int k=0; k<g; k++) {
+            for(int i=directions.size()-1; i>=0; i--) {
+                directions.add((directions.get(i) + 1) % 4);
             }
-            startGeneration++;
         }
     }
 
-    static void checkDot(int x, int y) {
+    static void setDragonCurve(int x, int y) {
         checked[y][x] = true;
 
-        for(int dot : dotList) {
+        for(int dot : directions) {
             int nx = x + dx[dot];
             int ny = y + dy[dot];
 
@@ -77,39 +55,16 @@ public class Main {
     }
 
     static int getDragonCurve() {
-        int result = 0;
-
+        int cnt = 0;
         for(int i=0; i<100; i++) {
             for(int j=0; j<100; j++) {
-                int cnt = 0;
-                if(checked[i][j]) cnt+= 1;
-                if(checked[i+1][j]) cnt+= 1;
-                if(checked[i][j+1]) cnt+= 1;
-                if(checked[i+1][j+1]) cnt+= 1;
-
-                if(cnt==4) result++;
+                if(checked[i][j] && checked[i+1][j] && checked[i][j+1] && checked[i+1][j+1]) cnt++;
             }
         }
-        return result;
+        return cnt;
     }
 
     static boolean outOfRange(int x, int y) {
         return x<0 || y<0 || x>100 || y>100;
-    }
-
-    static void checkMaybe(Dot dot) {
-        int x = dot.x; //3
-        int y = dot.y; //3
-        int d = dot.g; //방향 //1
-        int g = dot.g; //세대  //3
-
-        checked[y][x] = true;
-
-        for (int i=0; i<=g; i++){
-            int nx = x + dx[d+i]; // 3 + (0)
-            int ny = y + dy[d+i]; // 3 + (-1)
-
-            if(outOfRange(nx, ny)) continue;
-        }
     }
 }
