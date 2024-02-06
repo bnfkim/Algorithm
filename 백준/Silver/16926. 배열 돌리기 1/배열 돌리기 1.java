d94614e4fb8d95d36edefdf9846d85eaf289938f
Gyuri Kim <kgr2626@naver.com>
2024-02-06 13:52:20 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 732 ms, Memory: 30168 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -3,63 +3,72 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main{
-    static int n,m,r;
-    static int min;
+public class Main {
+
+    static int N, M, R;
     static int[][] arr;
-    //왼쪽으로 넣고, 위로넣고, 오른쪽으로 넣고, 아래로 넣는 순서
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken()); //세로
-        m = Integer.parseInt(st.nextToken()); //가로
-        r = Integer.parseInt(st.nextToken()); //회전
-        arr = new int[n][m];
 
-        //0) 배열 입력받기
-        for(int i=0; i<n; i++) {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); //세로
+        M = Integer.parseInt(st.nextToken()); //가로
+        R = Integer.parseInt(st.nextToken()); //회전
+
+        arr = new int[N][M];
+
+        for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<m; j++) {
+            for(int j=0; j<M; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        //1) 회전시켜야하는 라인의 갯수 구하기
-        int min = Math.min(n, m)/2; //돌아가는 라인
+        solve();
+        print();
+    }
+    static void solve() {
+        int min = Math.min(N, M)/2;
 
-        //2) 구한 라인 갯수만큼 반복하여 회전
-        for(int i=0; i<r; i++){ //회전 횟수만큼 반복
-            for(int j=0; j<min; j++) { //라인들 전부 돌리기
-                int x = j;
-                int y = j;
-                int tmp = arr[x][y]; //맨 마지막 값 넣어두기
+        int cnt = 0;
+        while(cnt != R) {
 
-                int idx = 0;
-                while(idx < 4) {
-                    int nx = x + dx[idx];
-                    int ny = y + dy[idx];
-                    //범위 안이라면
-                    if(nx < n-j && ny < m-j && nx>=j && ny>=j){
-                        //System.out.println("arr[" + x + ", " + y + "] (" + arr[x][y] + ") <- arr[" + nx + ", " + ny + "] (" + arr[nx][ny] + ")");
-                        arr[x][y] = arr[nx][ny];
-                        x = nx;
-                        y = ny;
+            for(int i=0; i<min; i++) {
+                int x = i;
+                int y = i;
+                int tmp = arr[y][x];
+
+                int dir = 0;
+                while (dir < 4) {
+                    int nx = x + dx[dir];
+                    int ny = y + dy[dir];
+
+                    if (nx < i || ny < i || nx >= M - i || ny >= N - i) {
+                        dir++; //범위에 벗어나면 방향 바꿈
+                        continue;
                     }
-                    //범위를 벗어났다면 다음 방향으로 이동
-                    else idx++;
+
+                    arr[y][x] = arr[ny][nx];
+                    y = ny;
+                    x = nx;
                 }
-                //빼놓은 값 넣어줌
-                arr[j+1][j] = tmp;
+                arr[i + 1][i] = tmp;
             }
-        }
-        //3) 출력하기
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++) System.out.print(arr[i][j] + " ");
-            System.out.println();
+            cnt++;
         }
     }
-
+    static void print() {
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<M; j++) {
+                sb.append(arr[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
 }
