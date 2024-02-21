ddbf468a2884a91e8dc58238b46d22e3948519f2
Gyuri Kim <kgr2626@naver.com>
2024-02-21 10:30:40 +0900

    [Silver I] Title: 영역 구하기, Time: 140 ms, Memory: 14544 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,63 +1,95 @@
-import org.w3c.dom.Node;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.util.stream.Collectors;
 
-class Main{
-    static int m, n, k; //세로, 가로, 직사각형 개수
-    static int[][] arr; //모눈종이
-    static int cnt = 0; //영역의 넓이를 구하기 위한 변수
-    static ArrayList<Integer> list = new ArrayList<>(); //각 영역의 넓이
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
+public class Main {
+    static int M,N,K;
+    static int[][] map;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken()); 
-        arr = new int[m][n];
-        for(int i=0; i<k; i++){
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        map = new int[M][N];
+
+        for(int i=0; i<K; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
+
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            //해당 모눈종이 채우기
-            for(int y=y1; y<y2; y++){
-                for(int x=x1; x<x2; x++) {
-                    arr[y][x] = 1;
-                }
-            }
+            drawSquare(x1, y1, x2, y2);
         }
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(arr[i][j] == 0) {
-                    cnt = 0;
-                    dfs(i, j);
-                    list.add(cnt);
-                }
+        ArrayList<Integer> answer = new ArrayList<>();
+        for(int i=0; i<M; i++) {
+            for(int j=0; j<N; j++) {
+                if(map[i][j] == 1) continue;
+                answer.add(getRange(j, i));
             }
         }
-        Collections.sort(list);
-        System.out.println(list.size()); //영역개수
-        for(int a : list) System.out.print(a + " ");
+        Collections.sort(answer);
+
+        sb.append(answer.size()).append("\n");
+        sb.append(answer.stream().map(Object::toString)
+                .collect(Collectors.joining(" ")));
+
+        System.out.println(sb);
     }
-    private static void dfs(int y, int x) {
-        arr[y][x] = 1;
-        cnt++;
 
-        for(int i=0; i<4; i++){
-            int nextX = x + dx[i];
-            int nextY = y + dy[i];
-            if(nextX<0 || nextX>=n || nextY<0 || nextY>=m) continue;
-            if(arr[nextY][nextX] == 0) dfs(nextY, nextX);
+    public static void drawSquare(int x1, int y1, int x2, int y2) {
+        for(int i=y1; i<y2; i++) {
+            for(int j=x1; j<x2; j++) {
+                map[i][j] = 1;
+            }
+        }
+    }
+
+    public static int getRange(int x, int y) {
+        int range = 0;
+
+        Deque<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{x, y});
+        map[y][x] = 1;
+
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+
+            for(int i=0; i<4; i++) {
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
+
+                if(nx<0 || ny<0 || nx>=N || ny>=M) continue;
+                if(map[ny][nx] == 1) continue;
+
+                queue.add(new int[]{nx, ny});
+                map[ny][nx] = 1;
+
+            }
+            range++;
+        }
+
+        return range;
+    }
+
+    public static void print() {
+        for(int i=0; i<M; i++) {
+            for(int j=0; j<N; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
