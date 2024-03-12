9c1ddd4a20cd8455b01279679433093c1c57bdc0
Gyuri Kim <kgr2626@naver.com>
2024-03-12 20:11:05 +0900

    [level 3] Title: 베스트앨범, Time: 0.53 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Song implements Comparable<Song> {
     int n, g, p;
     String genre;
+    
     public Song(int n, int  g, int p, String genre) {
         this.n = n;
         this.g = g;
@@ -24,28 +25,25 @@ class Song implements Comparable<Song> {
 
 class Solution {
     static int N;
-    //1. 속한 노래가 많이 재생된 장르를 먼저 수록합니다.
-    //2. 장르 내에서 많이 재생된 노래를 먼저 수록합니다.
-    //3. 장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
     
     public ArrayList<Integer> solution(String[] genres, int[] plays) {
         N = genres.length;
         ArrayList<Song> songList = new ArrayList<>();
         ArrayList<Integer> answer = new ArrayList<>();
         
+        //많이 재생된 장르를 수록하기 위한 map
         HashMap<String, Integer> map = new HashMap<>();
         for(int i=0; i<N; i++) {
             map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
         }
         
+        //정렬하기 위해, Song class 만들어 정렬
         for(int i=0; i<N; i++) {
-            int g = map.get(genres[i]);
-            int p = plays[i];
-            songList.add(new Song(i, g, p, genres[i]));
+            songList.add(new Song(i, map.get(genres[i]), plays[i], genres[i]));
         }
-        
         Collections.sort(songList);
         
+        //장 많이 재생된 노래를 최대 두 개까지만 넣기 위한 cnt hashMap
         HashMap<String, Integer> cnt = new HashMap<>();
         for(String key : map.keySet()) {
             cnt.put(key, 0);
@@ -54,10 +52,10 @@ class Solution {
         for(int i=0; i<songList.size(); i++) {
             Song song = songList.get(i);
             
-            if(cnt.get(song.genre) == 2) continue;
-            
-            answer.add(songList.get(i).n);
-            cnt.put(song.genre, cnt.get(song.genre) + 1);
+            if(cnt.get(song.genre) < 2) {
+                answer.add(songList.get(i).n);
+                cnt.put(song.genre, cnt.get(song.genre) + 1);
+            }
         }
         return answer;
     }
