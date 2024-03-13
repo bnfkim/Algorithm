693bbc188fd051cc675179c289f23d2432d504b5
Gyuri Kim <kgr2626@naver.com>
2024-03-13 09:37:13 +0900

    [Silver IV] Title: 수 찾기, Time: 588 ms, Memory: 45288 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,53 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] arr;
-    static int binarySearch(int key){ //이진탐색
-
-        int left = 0;
-        int right = arr.length-1;
-
-        while(left <= right){
-            int mid = (left+right)/2;
-            if (key < arr[mid]){
-                right = mid-1;
-            } else if (key > arr[mid]){
-                left = mid+1;
-            } else {
-                return mid;
-            }
-        }
-        return -1;
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
 
-        //좌표값 넣기기
-        arr = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        for(int i=0; i<N; i++){
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        //정렬하기
         Arrays.sort(arr);
 
         int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i=0; i<M; i++){
-            if(binarySearch(Integer.parseInt(st.nextToken())) >= 0){
-                sb.append(1).append("\n");
-            } else {
-                sb.append(0).append("\n");
-            }
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<M; i++){
+            int key = Integer.parseInt(st.nextToken());
+            if(Arrays.binarySearch(arr, key) >= 0) sb.append(1);
+            else sb.append(0);
+            sb.append("\n");
         }
 
-        System.out.println(sb);
+        System.out.println(sb.toString().trim());
     }
 }
