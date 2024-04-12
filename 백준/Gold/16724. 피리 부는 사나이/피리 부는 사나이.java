0e46c2c343ea53bb2457049208444b61804fa753
Gyuri Kim <kgr2626@naver.com>
2024-04-12 16:09:24 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 340 ms, Memory: 25692 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
@@ -6,21 +6,25 @@ import java.util.StringTokenizer;
 public class Main {
 
   static int N, M, CNT;
-  static String[][] map;
+  static char[][] map;
   static boolean[][] visit;
   static boolean[][] finished;
+
+  static int[] dx = {0 , 0, -1, 1};
+  static int[] dy = {-1, 1, 0 , 0};
+  static char[] move = {'U', 'D', 'L', 'R'};
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
-    map = new String[N][M];
+    map = new char[N][M];
     visit = new boolean[N][M];
     finished = new boolean[N][M];
 
     for(int i = 0; i < N; i++) {
-      map[i] = br.readLine().split("");
+      map[i] = br.readLine().toCharArray();
     }
 
     for(int i = 0; i < N; i++) {
@@ -35,16 +39,17 @@ public class Main {
   public static void dfs(int x, int y) {
     visit[y][x] = true;
 
-    int nx = x, ny = y;
-    if(map[y][x].equals("D")) ny = y + 1;
-    if(map[y][x].equals("U")) ny = y - 1;
-    if(map[y][x].equals("R")) nx = x + 1;
-    if(map[y][x].equals("L")) nx = x - 1;
+    for(int i = 0; i < 4; i++) {
+      if(map[y][x] == move[i]) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
 
-    if(nx >= 0 && ny >= 0 && nx < M && ny < N) {
-      if(!visit[ny][nx]) dfs(nx, ny);
-      else {
-        if(!finished[ny][nx]) CNT++;
+        if(nx < 0 || ny < 0 && nx >= M && ny >= N) break;
+        if(!visit[ny][nx]) dfs(nx, ny);
+        else {
+          if(!finished[ny][nx]) CNT++;
+        }
+        break;
       }
     }
     finished[y][x] = true;
