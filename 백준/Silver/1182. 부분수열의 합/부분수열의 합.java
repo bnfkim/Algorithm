aed407f1aad1b6c6ef376c1c4c10467c6d993a02
Gyuri Kim <kgr2626@naver.com>
2025-07-14 00:07:58 +0900

    [Silver II] Title: 부분수열의 합, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,36 +1,37 @@
-import java.io.*;
-import java.math.BigInteger;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static int n,s;
-    public static int cnt = 0;
-    public static int[] nums;
+
+    static int N,S;
+    static int[] arr;
+    static int cnt = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken()); //정수의 개수
-        s = Integer.parseInt(st.nextToken()); //정수
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
 
-        nums = new int[n];
+        arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         dfs(0, 0);
-        
-        System.out.println((s==0) ? cnt-1 : cnt);
+        System.out.println(S==0 ? cnt-1 : cnt);
     }
-    public static void dfs(int dep, int sum) {
-        //모든 탐색 경로를 매번 결과에 추가;
-        if(dep == n) {
-            if(sum == s) {
+
+    public static void dfs (int sum, int dep) {
+        if(dep == N) {
+            if(sum == S) {
                 cnt++;
             }
             return;
         }
 
-        dfs(dep+1, sum + nums[dep]); //해당 값을 더하거나
-        dfs(dep+1, sum); //해당 값을 더하지 않거나
+        dfs(sum + arr[dep], dep+1); //해당 수열 선택
+        dfs(sum, dep+1); //해당 수열 미선택
     }
+
 }
